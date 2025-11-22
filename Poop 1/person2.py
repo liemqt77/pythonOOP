74ce726b3bb94b5677e3b3928a826d5d5d2384f0
liemqt77 <pietersonwill@gmail.com>
2025-11-22 14:31:21 +0700

    Test OOP

---
+++ Poop 1/person2.py
@@ -0,0 +1,5 @@
+from person import Person
+
+person1 = Person("Williem", 22, "Coding")
+person2 = Person("Liliana", 24, "Teacher")
+
---
+++ Poop 1/person3.py
@@ -0,0 +1,5 @@
+from person2 import person1
+from person2 import  person2
+
+print(person1.name, person1.age, person1.worked)
+print(person2.name, person2.age, person2.worked)
