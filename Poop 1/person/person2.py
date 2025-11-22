602e859cb85d90039135562e9044eb775da87db0
liemqt77 <pietersonwill@gmail.com>
2025-11-22 14:39:07 +0700

    Test OOP

--- Poop 1/person/person2.py
+++ Poop 1/person/person2.py
@@ -2,4 +2,5 @@ from person import Person
 
 person1 = Person("Williem", 22, "Coding")
 person2 = Person("Liliana", 24, "Teacher")
+person3 = Person("Ayudia", 25, "Admin")
 
--- Poop 1/person/person3.py
+++ Poop 1/person/person3.py
@@ -1,5 +1,7 @@
 from person2 import person1
 from person2 import  person2
+from person2 import person3
 
 print(person1.name, person1.age, person1.worked)
 print(person2.name, person2.age, person2.worked)
+print(person3.name, person3.age, person3.worked)
