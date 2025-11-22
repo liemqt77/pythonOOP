7b888cca714feecce7336e245152e758ffe31fb3
liemqt77 <pietersonwill@gmail.com>
2025-11-22 14:23:52 +0700

    Test OOP

--- Poop 1/main.py
+++ Poop 1/main.py
@@ -1,14 +1,25 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, hobbies):
         self.name = name
         self.age = age
+        self.hobbies = hobbies
 
-person1 = Person("Will", 18)
+person1 = Person("Will", 20, "Football")
 print(person1.name)
 print(person1.age)
-person2 = Person("Liem", 14)
+print(person1.hobbies)
+
+
+person2 = Person("Liem", 14, "Basketball")
 print(person2.name)
 print(person2.age)
+print(person2.hobbies)
+
+
+person3 = Person("Akira", 20, "Boxing")
+print(person3.name)
+print(person3.age)
+print(person3.hobbies)
 
 class Car:
     def __init__(self, make, model, year, color):
